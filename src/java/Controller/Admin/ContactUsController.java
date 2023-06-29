a371934f24ae8e11bb66af8d155ff2e0e4ed50ad
Phukk <phuchtde16066@fpt.edu.vn>
2023-06-29 22:00:20 +0700

    contact us

---
+++ src/java/Controller/Admin/ContactUsController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller.Admin;
+
+import DAO.MotelDAO;
+import DAO.NotificationDAO;
+import DAO.PayCarDAO;
+import Model.Account;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author hatha
+ */
+public class ContactUsController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Account acc = (Account) session.getAttribute("acc");
+        NotificationDAO noti = new NotificationDAO();
+        if (acc == null) {
+            response.sendRedirect("login.jsp");
+        } else {
+            String comment = request.getParameter("message");
+            String rscore = request.getParameter("rate");
+            String alert = comment+"/"+rscore;
+            noti.insertAlertForAdmin(alert, acc.getAccId(), 8, 1);
+            request.setAttribute("mess", "Thank You for contact Us!");
+            request.getRequestDispatcher("contact.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
